ee7a6c92d5f3ffe45c030f1c0047dff2b433eb30
leepuppychow <leepuppychow@gmail.com>
2018-06-25 01:04:08 -0600

    Created modules for each resource in vuex store

---
+++ src/store/rent.js
@@ -0,0 +1,30 @@
+const authHeaders = () => ({ authorization: localStorage.getItem('token_id') });
+const baseURL = 'http://localhost:3000/api/v1';
+
+const rent = {
+  actions: {
+    setRent({ dispatch }, payload) {
+      const { propertyID, rent } = payload;
+      const body = {
+        amount: rent,
+      };
+
+      fetch(`${baseURL}/rent/${propertyID}`, {
+        method: 'PUT',
+        headers: {
+          ...authHeaders(),
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      })
+        .then(response => response.json())
+        .then((data) => {
+          dispatch('fetchProperties');
+          alert(data.message);
+        })
+        .catch(err => console.log(err));
+    },
+  },
+};
+
+export default rent;
