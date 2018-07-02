19ab2efd7a3cff2ffd1a99fb91976933f5dd72d5
leepuppychow <leepuppychow@gmail.com>
2018-07-01 23:41:01 -0700

    Adds testing for Property watchers

--- test/unit/specs/Property.spec.js
+++ test/unit/specs/Property.spec.js
@@ -35,14 +35,39 @@ describe('Property', () => {
 
   beforeEach(() => {
     wrapper = factory({}, { bills, tenants, property });
-  })
+  });
 
   it('can compute the current billTotal', () => {
     assert.equal(wrapper.vm.billTotal, 150);
   });
 
-  it('can compute the number of tenants', () => {
+  it('can compute the numberOfTenants', () => {
     assert.equal(wrapper.vm.numberOfTenants, 4);
-  })
+  });
+
+  it('on mount it will divideBillsAmongTenants', () => {
+    assert.equal(wrapper.vm.splitBillsAmount, 37.5);
+  });
+
+  // test Watchers
+  it('will update the splitBillAmount if the billTotal changes', () => {
+    wrapper.setProps({
+      bills: [
+        ...bills,
+        { id: 4, amount: 50 },
+      ],
+    });
+    assert.equal(wrapper.vm.splitBillsAmount, 50);
+  });
+  
+  it('will update the splitBillAmount if the numberOfTenants changes', () => {
+    wrapper.setProps({
+      tenants: [
+        ...tenants, 
+        { id: 5, name: 'Tobi' },
+      ],
+    })
+    assert.equal(wrapper.vm.splitBillsAmount, 30);
+  });
 });
 
