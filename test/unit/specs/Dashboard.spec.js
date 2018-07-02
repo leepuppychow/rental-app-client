ad34812bc29f84c5dfba84d9243242d967e4bd1e
leepuppychow <leepuppychow@gmail.com>
2018-07-02 00:38:31 -0700

    Adds more test to Dashboard component

--- test/unit/specs/Dashboard.spec.js
+++ test/unit/specs/Dashboard.spec.js
@@ -46,7 +46,13 @@ describe('Dashboard', () => {
 
   it('can get the activeProperty when component first mounts', () => {
     assert.deepEqual(wrapper.vm.activeProperty, properties[0]);
-  })
+  });
+
+  it("can setActiveTab", () => {
+    assert.equal(wrapper.vm.activeTab, null);
+    wrapper.vm.setActiveTab(2);
+    assert.equal(wrapper.vm.activeTab, 2);
+  });
 
   it('can get the activeProperty when a tab has been clicked', () => {
     assert.equal(wrapper.vm.activeTab, null);
@@ -59,6 +65,19 @@ describe('Dashboard', () => {
     tabs.at(2).trigger('click');
     assert.equal(wrapper.vm.activeTab, 3);
     assert.deepEqual(wrapper.vm.activeProperty, properties[2]);
+  });
+
+  it('can get bills that belong to this property', () => {
+    assert.equal(wrapper.vm.billsOfProperty(1).length, 2);
+    assert.equal(wrapper.vm.billsOfProperty(2).length, 1);
+    assert.equal(wrapper.vm.billsOfProperty(3).length, 0);
+  });
+
+  it('can get tenantOfProperty', () => {
+    assert.equal(wrapper.vm.tenantsOfProperty(3).length, 3);
+    assert.equal(wrapper.vm.tenantsOfProperty(1).length, 1);
+    assert.equal(wrapper.vm.tenantsOfProperty(2).length, 0);
+    assert.equal(wrapper.vm.tenantsOfProperty(1)[0].name, 'lee');
   })
 })
 ;
